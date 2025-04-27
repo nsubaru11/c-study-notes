195068417f25bd1b46fd05d6731a7a6d52d468ab
nsubaru11 <20051126subaru@gmail.com>
2025-04-28 01:31:06 +0900

    feat: Printerを実装しました。 出力内容を内部バッファに溜めて、最後に出力します。明示的にflushするか、メモリ解放時に出力されます。

--- Data Structures/HashMap/main.c
+++ Data Structures/HashMap/main.c
@@ -1,27 +1,25 @@
-#include <stdio.h>
-
+#include "printer.h"
 #include "string.h"
 
 int main() {
+    Printer *out = new_Printer();
     String *str = new_String_from_chars("abcdefghijklmnopqrstuvwxyz");
-    for (int i = 0; i < str->len; i++) {
-        printf("%c", str->array[i]);
-    }
-    printf("\n");
-    printf("%d\n", str->hashCode);
+    out->println_string(out, str);
+    out->println_int(out, str->hashCode);
     String *str2 = new_String();
     for (char i = 'a'; i <= 'z'; i++) {
         str2->append(str2, i);
     }
-    printf("%d\n", str2->hashCode);
+    out->println_int(out, str2->hashCode);
     String *str3 = new_String();
     str3->concat_chars(str3, "abc");
     str3->concat_chars(str3, "defghijklmnopqrstuvwxyz");
-    printf("%d\n", str3->hashCode);
-    printf("%d\n", equals(str, str2));
-    printf("%d\n", equals(str, str3));
+    out->println_int(out, str3->hashCode);
+    out->println_boolean(out, equals(str, str2));
+    out->println_boolean(out, equals(str, str3));
     free_String(str);
     free_String(str2);
     free_String(str3);
+    free_Printer(out);
     return 0;
 }
--- Data Structures/HashMap/printer.c
+++ Data Structures/HashMap/printer.c
@@ -2,28 +2,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 Printer *new_Printer() {
     Printer *out = malloc(sizeof(Printer));
-    out->BUFFER_SIZE = 1024;
+    out->buffer_size = 1024;
     out->pos = 0;
-    out->buffer = malloc(out->BUFFER_SIZE);
-    out->methods = malloc(sizeof(PrinterMethods));
-
-    out->methods->new_Printer = new_Printer;
-    out->methods->free_Printer = free_Printer;
-    out->methods->print_char = print_char;
-    out->methods->print_string = print_string;
-    out->methods->print_int = print_int;
-    out->methods->print_long = print_long;
-    out->methods->print_double = print_double;
-    out->methods->println = println;
-    out->methods->println_char = println_char;
-    out->methods->println_string = println_string;
-    out->methods->println_int = println_int;
-    out->methods->println_long = println_long;
-    out->methods->println_double = println_double;
-
+    out->buffer = malloc(out->buffer_size);
+    out->free_Printer = free_Printer;
+    out->flush = flush;
+    out->print_char = print_char;
+    out->print_string = print_string;
+    out->print_chars = print_chars;
+    out->print_int = print_int;
+    out->print_long = print_long;
+    out->print_double = print_double;
+    out->print_boolean = print_boolean;
+    out->println = println;
+    out->println_char = println_char;
+    out->println_string = println_string;
+    out->println_chars = println_chars;
+    out->println_int = println_int;
+    out->println_long = println_long;
+    out->println_double = println_double;
+    out->println_boolean = println_boolean;
     return out;
 }
 
@@ -32,11 +34,18 @@ void free_Printer(Printer *out) {
         if (out->pos > 0) {
             fwrite(out->buffer, 1, out->pos, stdout);
         }
-        free(out->buffer);
-        free(out->methods);
-        free(out);
         fflush(stdout);
+        free(out->buffer);
+        free(out);
     }
+}
+
+void flush(Printer *out) {
+    if (out->pos > 0) {
+        fwrite(out->buffer, 1, out->pos, stdout);
+        out->pos = 0;
+    }
+    fflush(stdout);
 }
 
 void write(Printer *out) {
@@ -46,7 +55,7 @@ void write(Printer *out) {
 }
 
 void print_char(Printer *out, char c) {
-    if (out->pos + 1 == out->BUFFER_SIZE) {
+    if (out->pos + 1 >= out->buffer_size) {
         write(out);
     }
     out->buffer[out->pos++] = c;
@@ -54,120 +63,131 @@ void print_char(Printer *out, char c) {
 
 void print_string(Printer *out, const String *str) {
     for (int i = 0; i < str->len; i++) {
-        print_char(out, str->array[i]);
+        out->print_char(out, str->array[i]);
     }
 }
 
-inline void print_int(Printer *out, int n) {
-    if (out->pos + 11 >= out->BUFFER_SIZE) {
+void print_chars(Printer *out, const char *str) {
+    for (int i = 0; str[i] != 0; i++) {
+        out->print_char(out, str[i]);
+    }
+}
+
+void print_int(Printer *out, int n) {
+    if (out->pos + 11 >= out->buffer_size) {
         write(out);
     }
     if (n < 0) {
         out->buffer[out->pos++] = '-';
         n = -n;
     }
-    int start_pos = out->pos;
+    int l = out->pos;
     while (n > 0) {
-        out->buffer[out->pos++] = (char) ('0' + (n % 10));
+        out->buffer[out->pos++] = (char) (n % 10 + '0');
         n /= 10;
     }
-    int end_pos = out->pos - 1;
-    while (start_pos < end_pos) {
-        char temp = out->buffer[start_pos];
-        out->buffer[start_pos] = out->buffer[end_pos];
-        out->buffer[end_pos] = temp;
-        start_pos++;
-        end_pos--;
+    int r = out->pos - 1;
+    while (l < r) {
+        char temp = out->buffer[l];
+        out->buffer[l] = out->buffer[r];
+        out->buffer[r] = temp;
+        l++;
+        r--;
     }
 }
 
-inline void print_long(Printer *out, long long n) {
-    if (out->pos + 20 >= out->BUFFER_SIZE) {
+void print_long(Printer *out, long long n) {
+    if (out->pos + 20 >= out->buffer_size) {
         write(out);
     }
     if (n < 0) {
         out->buffer[out->pos++] = '-';
         n = -n;
     }
-    int start_pos = out->pos;
+    int l = out->pos;
     while (n > 0) {
-        out->buffer[out->pos++] = (char) ('0' + (n % 10));
+        out->buffer[out->pos++] = (char) (n % 10 + '0');
         n /= 10;
     }
-    int end_pos = out->pos - 1;
-    while (start_pos < end_pos) {
-        char temp = out->buffer[start_pos];
-        out->buffer[start_pos] = out->buffer[end_pos];
-        out->buffer[end_pos] = temp;
-        start_pos++;
-        end_pos--;
+    int r = out->pos - 1;
+    while (l < r) {
+        char temp = out->buffer[l];
+        out->buffer[l] = out->buffer[r];
+        out->buffer[r] = temp;
+        l++;
+        r--;
     }
 }
 
-inline void print_double(Printer *out, double n) {
-    if (out->pos + 30 >= out->BUFFER_SIZE) {
-        write(out);
+void print_double(Printer *out, double n, int scale) {
+    if (scale == 0) {
+        out->print_long(out, llround(n));
+        return;
     }
     if (n < 0) {
-        out->buffer[out->pos++] = '-';
+        out->print_char(out, '-');
         n = -n;
     }
-    long long int_part = (long long) n;
-    double frac_part = n - (double) int_part;
-    if (int_part == 0) {
-        out->buffer[out->pos++] = '0';
+    n += pow(10, -scale) / 2;
+    out->print_long(out, (long long) n);
+    if (out->pos + scale + 1 >= out->buffer_size) {
+        write(out);
+    }
+    out->buffer[out->pos++] = '.';
+    n -= (double) (long long) n;
+    while (scale-- > 0) {
+        n *= 10;
+        out->buffer[out->pos++] = (char) ((int) n + '0');
+        n -= (int) n;
+    }
+}
+
+void print_boolean(Printer *out, int b) {
+    if (b) {
+        out->print_chars(out, "true");
     } else {
-        int start_pos = out->pos;
-        while (int_part > 0) {
-            out->buffer[out->pos++] = (char) ('0' + (int_part % 10));
-            int_part /= 10;
-        }
-        int end_pos = out->pos - 1;
-        while (start_pos < end_pos) {
-            char temp = out->buffer[start_pos];
-            out->buffer[start_pos] = out->buffer[end_pos];
-            out->buffer[end_pos] = temp;
-            start_pos++;
-            end_pos--;
-        }
-    }
-    if (frac_part > 0) {
-        out->buffer[out->pos++] = '.';
-        for (int i = 0; i < 6; i++) {
-            frac_part *= 10;
-            int digit = (int) frac_part;
-            out->buffer[out->pos++] = (char) ('0' + digit);
-            frac_part -= digit;
-            if (frac_part < 0.000001) break;
-        }
+        out->print_chars(out, "false");
     }
 }
 
-inline void println(Printer *out) {
-    print_char(out, '\n');
+void println(Printer *out) {
+    out->print_char(out, '\n');
 }
 
-inline void println_char(Printer *out, char c) {
-    print_char(out, c);
-    println(out);
+void println_char(Printer *out, char c) {
+    out->print_char(out, c);
+    out->println(out);
 }
 
-inline void println_string(Printer *out, const String *str) {
-    print_string(out, str);
-    println(out);
+void println_string(Printer *out, const String *str) {
+    out->print_string(out, str);
+    out->println(out);
 }
 
-inline void println_int(Printer *out, int n) {
-    print_int(out, n);
-    println(out);
+void println_chars(Printer *out, const char *str) {
+    out->print_chars(out, str);
+    out->println(out);
 }
 
-inline void println_long(Printer *out, long long n) {
-    print_long(out, n);
-    println(out);
+void println_int(Printer *out, int n) {
+    out->print_int(out, n);
+    out->println(out);
 }
 
-inline void println_double(Printer *out, double n) {
-    print_double(out, n);
-    println(out);
+void println_long(Printer *out, long long n) {
+    out->print_long(out, n);
+    out->println(out);
+}
+
+void println_double(Printer *out, double n, int scale) {
+    out->print_double(out, n, scale);
+    out->println(out);
+}
+
+void println_boolean(Printer *out, int b) {
+    if (b) {
+        out->println_chars(out, "true");
+    } else {
+        out->println_chars(out, "false");
+    }
 }
